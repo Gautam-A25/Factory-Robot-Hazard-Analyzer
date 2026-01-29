c463f97f2b05514d39e32f833b10900abf15c24a
Gautam-A25 <aayushgautam2502@gmail.com>
2026-01-29 15:30:51 +0530

    Added: UC2 Accept robot hazard inputs and echo values

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
  *
  * Displays the system name.
  *
  * @author Aayush
- * @version 1.0
+ * @version 2.0
  */
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
-        System.out.println("Factory Robot Hazard Analyzer");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Arm precision:");
+        double armPrecision = sc.nextDouble();
+
+        System.out.println("Enter Worker Density:");
+        int workerDensity = sc.nextInt();
+
+        System.out.println("Enter Machinery State:");
+        String machineryState = sc.next();
+
+        System.out.println("Arm Precision: " + armPrecision);
+        System.out.println("Worker Density: " + workerDensity);
+        System.out.println("Machinery State: " + machineryState);
+
+        sc.close();
     }
 }
