6da6038df658050f13ed7ccc32e0c6d102e551b0
Gautam-A25 <aayushgautam2502@gmail.com>
2026-01-29 15:17:14 +0530

    Added: UC1 Display Factory Robot Hazard Analyzer message

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,14 @@
+/**
+ * Factory Robot Hazard Analyzer
+ *
+ * Displays the system name.
+ *
+ * @author Aayush
+ * @version 1.0
+ */
+public class FactoryRobotHazardAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
